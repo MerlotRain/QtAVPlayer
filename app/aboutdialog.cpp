c39f26696afd3dd094ad96cbe1ce69b69e711e83
qiyuxiang <1198477036@qq.com>
2023-09-04 15:31:45 +0800

    1.仓库清空重新开始。

--- app/aboutdialog.cpp
+++
@@ -1,6 +0,0 @@
-#include "aboutdialog.h"
-#include "ui_aboutdialog.h"
-#include <QClipboard>
-#include <QPushButton>
-#include <QSysInfo>
-
--- core/core_global.h
+++
@@ -1,7 +0,0 @@
-#include <QtCore/QtGlobal>
-
-#ifdef CORE_LIBRARY
-#define PENCILCORE_EXPORT Q_DECL_EXPORT
-#else
-#define PENCILCORE_EXPORT Q_DECL_IMPORT
-#endif
